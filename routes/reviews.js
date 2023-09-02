29d5b9ba6d660b0d936e1f8992dac6b4701cd28d
Samyak Jain <sj.samyak.5@gmail.com>
2023-09-02 23:13:27 +0530

    Authorization implemented

--- routes/reviews.js
+++ routes/reviews.js
@@ -8,22 +8,13 @@ const { reviewSchema } = require('../schemas.js');
 
 const Campground = require('../models/campground');
 const Review = require('../models/review.js')
+const { validateReview, isLoggedIn } = require('../middleware')
 
 
-
-const validateReview = (req, res, next) => {
-    const { error } = reviewSchema.validate(req.body)
-    if (error) {
-        const msg = error.details.map(el => el.message).join(',')
-        throw new ExpressError(result.error.details, 400)
-    } else {
-        next();
-    }
-}
-
-router.post('/', validateReview, catchAsync(async (req, res) => {
+router.post('/', isLoggedIn, validateReview, catchAsync(async (req, res) => {
     const campground = await Campground.findById(req.params.id);
     const review = new Review(req.body.review)
+    review.author = req.user._id;
     campground.reviews.push(review);
     await review.save();
     await campground.save();
--- seeds/index.js
+++ seeds/index.js
@@ -24,6 +24,7 @@ const seedDB = async () => {
         const random1000 = Math.floor(Math.random() * 1000);
         const price = Math.floor(Math.random() * 20) + 10;
         const camp = new Campground({
+            author: '64eda280a6f6ada337300a7f',
             location: cities[random1000].city + ", " + cities[random1000].state,
             title: sample(descriptors) + " " + sample(places),
             image: 'http://source.unsplash.com/collection/483251',
